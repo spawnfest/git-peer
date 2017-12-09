e66af0c1ed1d96add8ee2dbbd895f2c867bde05a
Olafur Arason <olafura@olafura.com>
2017-12-09 14:29:19 -0500

    Git repo setup helper

--- apps/daemon/test/test_helper.exs
+++ apps/daemon/test/test_helper.exs
@@ -1,1 +1,17 @@
 ExUnit.start()
+
+defmodule GitSetup do
+  def temp_name, do: "repo_" <> Base.encode16(:crypto.strong_rand_bytes(32))
+
+  def repo_path do
+    Path.join(System.tmp_dir!(), temp_name())
+  end
+
+  def create_repo(path) do
+    with :ok <- File.mkdir(path) do
+      {:ok, Gitex.Git.init(path)}
+    else
+      error -> {:error, error}
+    end
+  end
+end
