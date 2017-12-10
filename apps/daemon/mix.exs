db5cfb5526f0beb092b7f897078a0fd0e8d02a00
Olafur Arason <olafura@olafura.com>
2017-12-10 16:28:11 -0500

    Add connect logic

--- apps/daemon/mix.exs
+++ apps/daemon/mix.exs
@@ -26,7 +26,8 @@ defmodule GitPeer.Daemon.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:logger_lager_backend, "~> 0.1.0"}
+      {:logger_lager_backend, "~> 0.1.0"},
+      {:lasp, "~> 0.8.2"}
     ]
   end
 end
