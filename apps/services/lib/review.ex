53ef1c9ad4aee389c2200c5961be057bde6ac733
Olafur Arason <olafura@olafura.com>
2017-12-10 17:26:04 -0500

    Add service review

---
+++ apps/services/lib/review.ex
@@ -0,0 +1,40 @@
+defmodule GitPeer.Services.Review do
+
+  def request_review(%{requester: requester, ref: ref, users: user}) do
+    :lasp.update(
+      {"reviews", :state_orset},
+      %{requester: requester, ref: ref, users: user},
+      self()
+    )
+  end
+
+  def get_reviews do
+    with {:ok, review} when is_list(reviews) <-
+           :lasp.query({"reviews", :state_orset}) do
+      reviews
+      |> :sets.to_list()
+    else
+      {:error, error} -> {:error, error}
+      error -> {:error, error}
+    end
+  end
+
+  def review_response(%{requester: requester, ref: ref, reviewer: reviewer, review: review}) do
+    :lasp.update(
+      {"review_responses", :state_orset},
+      %{requester: requester, ref: ref, reviewer: reviewer, review: review}
+      self()
+    )
+  end
+
+  def get_reviews do
+    with {:ok, review} when is_list(reviews) <-
+           :lasp.query({"review_responses", :state_orset}) do
+      reviews
+      |> :sets.to_list()
+    else
+      {:error, error} -> {:error, error}
+      error -> {:error, error}
+    end
+  end
+end
