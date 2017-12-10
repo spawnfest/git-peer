66fc1e9429f4f3f630ddb74806acb41a2e4df1e6
Olafur Arason <olafura@olafura.com>
2017-12-10 17:26:15 -0500

    Add Daemon functions

--- apps/daemon/lib/daemon.ex
+++ apps/daemon/lib/daemon.ex
@@ -2,4 +2,30 @@ defmodule GitPeer.Daemon do
   @moduledoc """
   Documentation for GitPeer.Daemon.
   """
+
+  alias GitPeer.Services
+
+  def add_user(%{"user" => user, "key" => key}) do
+    Services.User.set_user(%{"user" => user, "key" => key})
+  end
+
+  def get_users do
+    Services.User.get_users()
+  end
+
+  def request_review(%{requester: requester, ref: ref, users: user}) do
+    Services.Review.request_review(%{requester: requester, ref: ref, users: user})
+  end
+
+  def get_reviews do
+    Services.Review.get_reviews()
+  end
+
+  def review_response(%{requester: requester, ref: ref, reviewer: reviewer, review: review}) do
+    Services.Review.review_response(%{requester: requester, ref: ref, reviewer: reviewer, review: review})
+  end
+
+  def get_reviews do
+    Services.Review.get_reviews()
+  end
 end
