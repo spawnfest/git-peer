01911ecdeb996749d205abc1ea66cf7311fc876e
Olafur Arason <olafura@olafura.com>
2017-12-10 17:33:07 -0500

    Move getting email and key to cli

--- apps/services/lib/user.ex
+++ apps/services/lib/user.ex
@@ -3,24 +3,6 @@ defmodule GitPeer.Services.User do
   User services
   """
 
-  def get_user_and_public_key do
-    public_key = Path.expand("~/.ssh/id_rsa.pub")
-
-    with {:ok, public_key_content} when is_binary(public_key_content) <-
-           File.read(public_key),
-         public_key_content <- String.trim(public_key_content),
-         %{"key" => _, "user" => _} = result <-
-           Regex.named_captures(
-             ~r/^ssh-rsa (?<key>\S+) (?<user>\S+)$/,
-             public_key_content
-           ) do
-      {:ok, result}
-    else
-      {:error, error} -> {:error, error}
-      error -> {:error, error}
-    end
-  end
-
   def set_user(%{"user" => user, "key" => key}) do
     :lasp.update(
       {"users", :state_awmap},
